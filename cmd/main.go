cde56c93d7047ffef11a0d99bfc3b52548a9fe43
Wellington Lopes <wellington.lopes.tw@gmail.com>
2023-05-13 15:21:02 -0300

    new relic test

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"github.com/newrelic/go-agent/v3/newrelic"
 	"github.com/wellingtonlope/todo-heroku-api/internal/app/usecase"
 	http2 "github.com/wellingtonlope/todo-heroku-api/internal/infra/http"
 	"github.com/wellingtonlope/todo-heroku-api/internal/infra/memory"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
@@ -17,6 +19,21 @@ func main() {
 	if err := godotenv.Load(); err != nil && shouldReadEnvFile() {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
+
+	app, err := newrelic.NewApplication(
+		newrelic.ConfigAppName("todo-api-stage"),
+		newrelic.ConfigLicense(os.Getenv("NEW_RELIC_LICENSE_KEY")),
+		newrelic.ConfigAppLogForwardingEnabled(true),
+	)
+	if err != nil {
+		log.Fatalf("Error loading new relic file: %v", err)
+	}
+	app.RecordLog(newrelic.LogData{Message: "teste", Severity: "error"})
+	app.RecordCustomEvent("just_a_test", map[string]interface{}{"name": "test"})
+	trs := app.StartTransaction("a_transaction")
+	time.Sleep(time.Second)
+	trs.End()
+	app.RecordCustomMetric("just_a_metric", 1.2)
 
 	memoryRepository := memory.NewTodo()
 	createTodo := usecase.NewCreateTodo(memoryRepository)
--- go.mod
+++ go.mod
@@ -9,13 +9,18 @@ require (
 )
 
 require (
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/newrelic/go-agent/v3 v3.21.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
-	golang.org/x/net v0.7.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
+	google.golang.org/grpc v1.54.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
